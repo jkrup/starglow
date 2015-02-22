0ff40c8ec90509ce597aed25886f76a9f9e40581
Justin Krup <mazlix@gmail.com>
2015-02-22 16:41:27 -0500

    renamed mutebutton to muteButton, fixed js and put it in Template events

--- client/templates/mute-button.js
+++ client/templates/mute-button.js
@@ -1,15 +1,12 @@
-$(document).ready(function () {
-
-      $(document).on('click', '.muted',
-      	function () {
-      		$('muted').addClass('playing').removeClass('.muted');
-			bgm.unmute();
-      	});
-
-      $(document).on('click', '.muted',
-      	function () {
-      		$('playing').addClass('muted').removeClass('.playing');
-			bgm.mute();
-      	});
-
-  });
+// instead of in $(document).ready(... we just put Template.____.events({
+Template.muteButton.events({
+  'touchend.fingers .muted': function() {
+    console.log(this);
+    $('.muted').addClass('playing').removeClass('muted');
+    bgm.unmute();
+  },
+  'touchend.fingers .playing': function() {
+    $('.playing').addClass('muted').removeClass('playing');
+    bgm.mute();
+  }
+})
